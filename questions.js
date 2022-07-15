78de51a655cc6aa4b0e226427c0c5c79aece2ff5
Jon <jonathancerruti@gmail.com>
2022-07-15 00:51:10 -0700

    on click instruction removed and question conatiner displays

--- questions.js
+++ questions.js
@@ -1,42 +1,31 @@
 // questions 
-let questions = [
+var questions = [
     {
-        question : "How many horns did the triceratops have?",
-        choiceA : "1",
-        choiceB : "2",
-        choiceC : "3",
-        choiceD : "4",
-        correct : "C"
-    },{
-        question : "What dinosaur themed book was turned into a blockbuster movie in 1993??",
-        choiceA : "Dinosaur Park",
-        choiceB : "Jurassic Park",
-        choiceC : "Camp Cretacious",
-        choiceD : "Triassic Park",
-        correct : "B"
-    },{
-        question : "What dinosaur species did the character Owen Grady train in film Jurassic World??",
-        choiceA : "Tyrannosaurus Rex",
-        choiceB : "Dilophosaurus",
-        choiceC : "Spinosaurus",
-        choiceD : "Velociraptor",
-        correct : "D"
-    },{
-        question : "How many teeth did a Tyrannosaurus Rex have?",
-        choiceA : "Spinosaurus",
-        choiceB : "'Brachiosaurus",
-        choiceC : "Ankylosaurus",
-        choiceD : "Tyrannosaurus Rex",
-        correct : "A"
-    },{
-        question : "How many teeth did a Tyrannosaurus Rex have?",
-        choiceA : "60",
-        choiceB : "20",
-        choiceC : "32",
-        choiceD : "100",
-        correct : "A"
+        title : "How many horns did the triceratops have?",
+        choices : ["1","2","3","4"],
+        answer : "3"
+    },
+    {
+        title : "What dinosaur themed book was turned into a blockbuster movie in 1993??",
+        choices : ["Dinosaur Park","Jurassic Park","Camp Cretacious","Triassic Park",],
+        answer : "Jurassic Park"
+    },
+    {
+        title : "What dinosaur species did the character Owen Grady train in film Jurassic World??",
+        choiceA : ["Tyrannosaurus Rex","Dilophosaurus","Spinosaurus","Velociraptor"],
+        answer: "D"
+    },
+    {
+        title : "How many teeth did a Tyrannosaurus Rex have?",
+        choices : ["Spinosaurus","Brachiosaurus","Ankylosaurus","Tyrannosaurus Rex"],
+        answer: "Spinosaurus"
+    },
+    {
+        title : "How many teeth did a Tyrannosaurus Rex have?",
+        choices : ["60","20","32","100"],
+        answer : "60"
     }
-];
+]
 
 
 
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const quizQestions = document.getElementById('quiz');
-const start = document.getElementById('start-btn');
-const infoBox = document.getElementById('info-title');
-const timer = document.getElementById('timer');
-const counter = document.getElementById('counter');
-const timeGuage = document.getElementById('timeGuage');
-const choices = document.getElementById('choices');
-const A = document.getElementById('A');
-const B  = document.getElementById('B');
-const C = document.getElementById('C');
-const D  = document.getElementById('D');
-const feedBack = document.getElementById('feedback');
-const question = document.getElementById('question');
+var quizQestions = document.getElementById('quiz');
+var start = document.getElementById('start-btn');
+var infoBox = document.getElementById('info-title');
+var timer = document.getElementById('timer');
+var counter = document.getElementById('counter');
+var timeGuage = document.getElementById('timeGuage');
+var choices = document.getElementById('choices');
+var A = document.getElementById('A');
+var B  = document.getElementById('B');
+var C = document.getElementById('C');
+var D  = document.getElementById('D');
+var feedBack = document.getElementById('feedback');
+var question = document.getElementById('quiz');
 
 start.addEventListener('click',renderQuestions );
 
 function countdown(minus) {
     var time = 75;
   
-    // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
     if (time >0){
       timer.textContent = time
@@ -37,5 +37,8 @@ function countdown(minus) {
 
 function renderQuestions(){
   countdown();
-    question.textContent = 'What is your name?'
+    question.style.display = "block"
+    infoBox.style.display = "none"
+    // var currentQuestion = questions[currentQuestionIndex];
+  
 }
